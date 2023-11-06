d24fa734938d3e6ba38dcfdc6386ebd0c36dabc5
Aleesha Naxakis <aleesha-@hotmail.com>
2023-11-06 15:49:41 +1100

    added delete routes

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const path = require('path');
 const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const {
+    readFromFile,
+    readAndAppend,
+    writeToFile,
+  } = require('./fsUtils');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -32,9 +37,26 @@ app.post('/api/notes', (req, res) => {
   res.json(newNote);
 });
 
+// DELETE Route 
+app.delete('/api/notes/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    readFromFile('db/db.json')
+      .then((data) => JSON.parse(data))
+      .then((json) => {
+        // Make a new array of all tips except the one with the ID provided in the URL
+        const result = json.filter((note) => note.id !== id);
+        console.log(result);
+        // Save that array to the filesystem
+        writeToFile('db/db.json', result);
+  
+        // Respond to the DELETE request
+        res.json(result);
+      });
+  });
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
-  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
