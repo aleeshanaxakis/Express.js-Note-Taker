1c45bc69fea8d46bebc9fffeef8b4b4278c07ea2
Aleesha Naxakis <aleesha-@hotmail.com>
2023-11-06 21:55:40 +1100

    added comments

--- server.js
+++ server.js
@@ -1,25 +1,33 @@
+// Import required modules and initialise Express
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Import utility functions for the file operations
 const {
     readFromFile,
     readAndAppend,
     writeToFile,
   } = require('./fsUtils');
 
+// Middleware 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
 // HTML Routes
+
+// Route to serve notes.html 
 app.get('/notes', (req, res) => {
   console.log('Accessed /notes route');
   res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
 // API Routes
+
+// Route to get notes from db.json
 app.get('/api/notes', (req, res) => {
   const dbFilePath = 'db/db.json';
   const notes = JSON.parse(fs.readFileSync(dbFilePath, 'utf8'));
@@ -27,6 +35,7 @@ app.get('/api/notes', (req, res) => {
   res.json(notes);
 });
 
+// Route to add a new note to db.json
 app.post('/api/notes', (req, res) => {
   const dbFilePath = 'db/db.json';
   const notes = JSON.parse(fs.readFileSync(dbFilePath, 'utf8'));
@@ -38,12 +47,14 @@ app.post('/api/notes', (req, res) => {
 });
 
 // DELETE Route 
+
+// Route to delete a note from db.json
 app.delete('/api/notes/:id', (req, res) => {
     const id = parseInt(req.params.id);
     readFromFile('db/db.json')
       .then((data) => JSON.parse(data))
       .then((json) => {
-        // Make a new array of all tips except the one with the ID provided in the URL
+        // Make a new array of all notes except the one with the ID provided in the URL
         const result = json.filter((note) => note.id !== id);
         console.log(result);
         // Save that array to the filesystem
@@ -54,10 +65,12 @@ app.delete('/api/notes/:id', (req, res) => {
       });
   });
 
+// Catch-all route to serve index.html for any other routes
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
+// Start the server and listen on the specified port
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
